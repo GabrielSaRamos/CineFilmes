bdbd9f51dcca0c5f0f1d3927b42fa762a06dc30e
Gabriel Ramos <ga.sa.ramos@hotmail.com>
2025-03-04 21:51:02 -0300

    Ajustando barra de pesquisa

--- movies-app/src/components/BarraPesquisa/index.tsx
+++ movies-app/src/components/BarraPesquisa/index.tsx
@@ -1,30 +1,61 @@
 "use client";
 import styled from "styled-components";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const SearchBar = styled.div`
+
+const SearchInput = styled.input`
+  border: none;
+  background: none;
+  outline: none;
+  float: left;
+  padding: 0;
+  color: #fff;
+  font-size: 16px;
+  transition: 0.4s;
+  line-height: 40px;
+  width: 0px;
+`;
+
+const Botao = styled.a`
+ color: #00a8ff;
+ float: right;
+ width: 40px;
+ height: 40px;  
+ border-radius: 50%;
+ background: #353b48;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ transition: 2s;
+`;
+
+const SearchBarContainer = styled.div`
     display: flex;
-    gap: 30px;
-    input {
-        padding: 5px;
-        border-radius: 5px;
-        border: 1px solid #ccc;
-        
-    }
-    button {
-        padding: 5px 10px;
-        border-radius: 5px;
-        border: 1px solid #ccc;
-        background-color: #f5f5f5;
-        cursor: pointer;
-        
-    }
-`
-export default function BarraPesquisa() {
+    padding: 10px;
+    border-radius: 40px;
+    height: 40px;
+    background: #353b48;
+    color: white;
 
+    &:hover ${SearchInput} {
+        width: 240px; /* Ajuste o valor conforme necessário */
+        padding: 0 6px;
+    }
+
+    &:hover ${Botao} {
+        background: white;
+
+    }
+`;
+
+
+
+export default function BarraPesquisa() {
     return (
-        <SearchBar>
-            <input type="text" placeholder="Pesquise seu filme" />
-            <button>Pesquisar</button>
-         </SearchBar>
-    )
+        <SearchBarContainer>
+            <SearchInput placeholder="Digite sua busca!" />
+            <Botao><i className="fas fa-search"></i></Botao>
+            
+        </SearchBarContainer>
+    );
 }
